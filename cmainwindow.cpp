cc6836f0b09750b4f57ba50a061a1f539e0a8c59
pauly.joerg <pauly.joerg@googlemail.com>
2014-03-31 10:32:10 +0000

    Init

---
+++ cmainwindow.cpp
@@ -0,0 +1,36 @@
+/****************************************************************************************
+*
+*   This file is part of the ChartButler Project.
+*   Copyright (C) 2014 Joerg Pauly
+*   Created 31.03.2014 by joerg
+*   All Rights reserved
+*
+*   ChartButler ist Freie Software: Sie können es unter den Bedingungen
+*   der GNU Lesser General Public License, wie von der Free Software Foundation,
+*   Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+*   veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+*
+*   Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+*   OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+*   Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+*   Siehe die GNU Lesser General Public License für weitere Details.
+*
+*   Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+*   Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************************/
+
+#include "cmainwindow.h"
+#include "ui_cmainwindow.h"
+
+CMainWindow::CMainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::CMainWindow)
+{
+    ui->setupUi(this);
+}
+
+CMainWindow::~CMainWindow()
+{
+    delete ui;
+}
