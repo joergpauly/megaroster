0af4bcba32a8f6c4b3bc9d06de39e2ca49a65410
pauly.joerg@googlemail.com <pauly.joerg@googlemail.com@1c11eb44-7fb9-d06c-46a9-831200b49def>
2014-04-14 06:49:10 +0000

    Anlage Datenklassen für PreAlerts

---
+++ cprealerttype.cpp
@@ -0,0 +1,27 @@
+/****************************************************************************************
+*
+*   This file is part of the MEGA-Series Project.
+*   Copyright (C) 2014 Joerg Pauly
+*   Created 14.04.2014 by joerg
+*   All Rights reserved
+*
+*   Alle Programme der MEGA-Serie sind Freie Software: Sie können sie unter den Bedingungen
+*   der GNU Lesser General Public License, wie von der Free Software Foundation,
+*   Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+*   veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+*
+*   Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+*   OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+*   Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+*   Siehe die GNU Lesser General Public License für weitere Details.
+*
+*   Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+*   Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************************/
+
+#include "cprealerttype.h"
+
+CPrealertType::CPrealertType()
+{
+}
