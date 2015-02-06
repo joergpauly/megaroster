fc9f70825fae646e10c9bc50e455022f70364182
pauly.joerg <pauly.joerg@googlemail.com>
2015-02-06 06:52:30 +0000

    Anlage CSinglePrealertEdit

---
+++ csingleprealertedit.h
@@ -0,0 +1,46 @@
+/****************************************************************************************
+*
+*   File: csingleprealertedit.h
+*
+*   This file is part of the MEGASeries Project.
+*   Copyright (C) 2015 Joerg Pauly, MEGAMover.de
+*   File created 06.02.2015 by joerg
+*   All Rights reserved
+*
+*   Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+*   der GNU Lesser General Public License, wie von der Free Software Foundation,
+*   Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+*   veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+*
+*   Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+*   OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+*   Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+*   Siehe die GNU Lesser General Public License für weitere Details.
+*
+*   Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+*   Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************************/
+
+#ifndef CSINGLEPREALERTEDIT_H
+#define CSINGLEPREALERTEDIT_H
+
+#include <QDialog>
+
+namespace Ui {
+class CSinglePrealertEdit;
+}
+
+class CSinglePrealertEdit : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CSinglePrealertEdit(QWidget *parent = 0);
+    ~CSinglePrealertEdit();
+
+private:
+    Ui::CSinglePrealertEdit *ui;
+};
+
+#endif // CSINGLEPREALERTEDIT_H
