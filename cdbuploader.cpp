2a3fcfbce17cae4a3ee6d9daf93c9f5dcb879db4
joergpauly <joerg.pauly@megamover.de>
2016-01-06 08:48:38 +0100

    Upload

--- cdbuploader.cpp
+++ cdbuploader.cpp
@@ -1,3 +1,30 @@
+/********************************************************************
+*
+*   File: cdbuploader.cpp    Class: CDbUploader
+*
+*   This file is part of the MEGA-Series Project.
+*   Copyright (C) 2016 Joerg Pauly
+*   Created 05.01.2015 by joerg
+*   All Rights reserved
+*
+*   Alle Programme der MEGA-Serie sind Freie Software:
+*   Sie können sie unter den Bedingungen
+*   der GNU Lesser General Public License, wie von der
+*   Free Software Foundation, Version 3 der Lizenz oder
+*   (nach Ihrer Option) jeder späteren veröffentlichten
+*   Version, weiterverbreiten und/oder modifizieren.
+*
+*   Dieses Programm wird in der Hoffnung, dass es nützlich
+*   sein wird, aber OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt;
+*   sogar ohne die implizite Gewährleistung der MARKTFÄHIGKEIT
+*   oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+*   Siehe die GNU Lesser General Public License für weitere Details.
+*
+*   Sie sollten eine Kopie der GNU Lesser General Public License
+*   zusammen mit diesem Programm erhalten haben.
+*   Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*
+********************************************************************/
 #include "cdbuploader.h"
 #include "cmainwindow.h"
 
